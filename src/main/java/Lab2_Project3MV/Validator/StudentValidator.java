add078ae477338813e60ade855a596697cea30bb
Corina Todoran <todorananacorina13@gmail.com>
2019-03-24 14:59:15 +0200

    add student name and teacher name tested

--- src/main/java/Lab2_Project3MV/Validator/StudentValidator.java
+++ src/main/java/Lab2_Project3MV/Validator/StudentValidator.java
@@ -12,21 +12,38 @@ public class StudentValidator implements IValidator<Student> {
         }
 
         if(!s.getId().matches("[+-]?[0-9][0-9]*[.]?[0-9][0-9]*")){
-            throw new ValidatorException("Id invalid number\n");
+            throw new ValidatorException("Id: invalid number\n");
             //errors+="Id invalid number\n";
         }
         if(!s.getId().matches("[0-9][0-9]*")){
-            throw new ValidatorException("Id invalid natural number\n");
+            throw new ValidatorException("Id: not a natural number\n");
             //errors+="Id invalid natural number\n";
         }
         if(Integer.parseInt(s.getId()) > 9999 || Integer.parseInt(s.getId()) < 1000){
-            throw new ValidatorException("Id invalid boundaries\n");
+            throw new ValidatorException("Id: invalid boundaries\n");
             //errors+="Id invalid natural number\n";
         }
+
         if(s.getNume().equals("") || s.getNume()==null){
-            //throw new ValidatorException("Nume invalid\n");
-            errors+="Nume invalid\n";
+            throw new ValidatorException("Name: should not be empty\n");
+            //errors+="Nume invalid\n";
         }
+
+        if (!s.getNume().matches("^[\\p{L} .'-]+$")) {
+            throw new ValidatorException("Name: invalid\n");
+            //errors+="Nume invalid\n";
+        }
+
+        if(s.getIndrumator().equals("") || s.getIndrumator()==null){
+            throw new ValidatorException("Teacher: should not be empty\n");
+            //errors+="Nume invalid\n";
+        }
+
+        if (!s.getIndrumator().matches("^[\\p{L} .'-]+$")) {
+            throw new ValidatorException("Teacher: invalid\n");
+            //errors+="Nume invalid\n";
+        }
+
         if(s.getGrupa()<=0){
             //throw new ValidatorException("Grupa invalida\n");
             errors+="Grupa invalid\n";
