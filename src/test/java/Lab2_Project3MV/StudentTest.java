97bd356b4af113f893a91434d747fffa15ec67a7
Corina Todoran <todorananacorina13@gmail.com>
2019-03-24 12:28:35 +0200

    added empty test case

---
+++ src/test/java/Lab2_Project3MV/StudentTest.java
@@ -0,0 +1,12 @@
+package Lab2_Project3MV;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class StudentTest {
+    @Test
+    public void addStudent() {
+        assertTrue(true);
+    }
+}
