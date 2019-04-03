4ec347714c17c985269d00fd4a9949c241ba9d3c
Gabi Crisan <gabrielcrisan97@yahoo.com>
2019-04-03 13:13:26 +0300

    Added Test cases for addAssign

--- src/test/java/Lab2_Project3MV/AppTest.java
+++ src/test/java/Lab2_Project3MV/AppTest.java
@@ -1,7 +1,14 @@
 package Lab2_Project3MV;
 
+import static junit.framework.TestCase.fail;
 import static org.junit.Assert.assertTrue;
 
+import Lab2_Project3MV.Exceptions.ValidatorException;
+import Lab2_Project3MV.Repository.XMLFileRepository.TemaLabXMLRepo;
+import Lab2_Project3MV.Service.XMLFileService.TemaLabXMLService;
+import Lab2_Project3MV.Validator.TemaLabValidator;
+import junit.framework.TestCase;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -9,12 +16,38 @@ import org.junit.Test;
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+    private TemaLabValidator validator;
+    private TemaLabXMLRepo xmlRepo;
+    private TemaLabXMLService xmlService;
+
+    @Before
+    public void setUp(){
+        validator = new TemaLabValidator();
+        xmlRepo = new TemaLabXMLRepo(validator, "assign-test.xml");
+        xmlService = new TemaLabXMLService(xmlRepo);
+    }
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void addHomeworkValid() {
+        String[] params={"asa", "Ana", "932", "Zsu"};
+        try {
+            xmlService.add(params);
+            fail();
+        } catch (ValidatorException e) {
+            System.out.println(e.getMessage());
+            TestCase.assertTrue(true);
+        }
+    }
+
+    @Test
+    public void addHomeworkInvalid() {
+        String[] params={"asa", "Ana", "932", "Zsu"};
+        try {
+            xmlService.add(params);
+            fail();
+        } catch (ValidatorException e) {
+            System.out.println(e.getMessage());
+            TestCase.assertFalse(false);
+        }
     }
 }
